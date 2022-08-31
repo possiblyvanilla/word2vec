059cba1a35942ce20533f37fd9405a70038241a4
Harish Shan <112258855+possiblyvanilla@users.noreply.github.com>
2022-08-31 23:02:00 +0530

    Added initial files

---
+++ Makefile
@@ -0,0 +1,12 @@
+CC = gcc-12
+CFLAGS = -lm -Ofast -march=native -funroll-loops
+
+all: word2vec distance
+
+word2vec : word2vec.c
+	$(CC) word2vec.c -o word2vec $(CFLAGS)
+distance : distance.c
+	$(CC) distance.c -o distance $(CFLAGS)
+
+clean:
+	rm -rf word2vec distance
---
+++ demo-word.sh
@@ -0,0 +1,7 @@
+make
+if [ ! -e text8 ]; then
+  wget http://mattmahoney.net/dc/text8.zip -O text8.gz
+  gzip -d text8.gz -f
+fi
+time ./word2vec -train text8 -output vectors.bin -cbow 0 -size 100 -window 5 -negative 5 -hs 1 -sample 1e-4 -threads 1 -binary 1 -iter 5
+./distance vectors.bin
